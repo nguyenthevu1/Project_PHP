7d4fb3921a49d7eb9d9ace96a05868c41f92f200
chithanh2001 <satthumaulanh2001@gmail.com>
2021-10-26 17:24:45 +0700

    update trang chi tiet

--- client/detail.php
+++ client/detail.php
@@ -6,33 +6,136 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
 </head>
 <body>
-    <div class="header">
-        <div class="container">
-            <div class="image">
+    <div class="container">
+        <div class="header">
+            <div class="container-header">
+                <div class="image">
 
-            </div>
-            <div class="list">
+                </div>
+                <div class="list">
+                    <ul>
+                        <li class="modify">
+                            <a href="#"><h2> Khách sạn<h2></a>                       
+                        </li>
+                        <li class="modify">
+                            <a href="#"><h2>Tours<h2></a>
+                        </li>
+                        <li class="modify">
+                            <a href="#"><h2>Vé máy bay</h2></a>
+                        </li>
+                        <li class="modify">
+                            <a href="#"><h2>Ăn uống</h2></a>
+                        </li>
+                        <li class="modify">
+                            <h2>...</h2>
+                        </li>           
+                    </ul>
+                </div>
+                <div class="right-header">
+                    <ul>
+                        <li class="modify">
+                            <a>
+                                <i class="bi bi-search"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>   
+            </div>         
+        </div>
+        <div class="hidden">
+            <section class="container-hidden">
                 <ul>
-                    <li class="modify">
-                        <a href="#"><h2> Khách sạn<h2></a>                       
+                    <li>
+                        <a href=#>
+                            <span class="boder">Trang chủ</span>
+                        </a>
                     </li>
-                    <li class="modify">
-                        <a href="#"><h2>Tours<h2></a>
+
+                    <li>
+                        <a href=#>
+                            <span class="boder">Việt Nam</span>
+                        </a>
                     </li>
-                    <li class="modify">
-                        <a href="#"><h2>Vé máy bay</h2></a>
+
+                    <li>
+                        <a href=#>
+                            <span class="boder">Sapa</span>
+                        </a>
                     </li>
-                    <li class="modify">
-                        <a href="#"><h2>Ăn uống</h2></a>
-                    </li>
-                    <li class="modify">
-                        <h2>...</h2>
+
+                    <li class="active">
+                        <span>Khu nghỉ dưỡng</span>                      
                     </li>
                 </ul>
-            </div>
+            </section>
+        </div>
+        <div class="container-body">
+            <section class="body-section">
+                <div class="body-left">
+                    <div class="detail-left">
+                        <div class="tourPanelHeading">
+                            <h4>Khách sạn tương tự</h4>
+                        </div>
+
+                        <div class="hotel-left">
+                            <div class="hotel-one">
+                                <span class="media-left">
+                                    <a href="#">
+                                        <img class="img-rounded" src="images/hinhdaidien-60x45.jpg">
+                                    </a>
+                                </span>
+                                <div class="media-body">
+                                    <h4>
+                                        <a href="#">
+                                            Khách sạn Bambo Sapa
+                                            <div class="star">
+                                                <i class="bi bi-star-fill star1"></i>
+                                                <i class="bi bi-star-fill"></i>
+                                                <i class="bi bi-star-fill"></i>
+                                                <i class="bi bi-star-fill"></i>
+                                            </div>                                                                                  
+                                        </a>
+                                    </h4>     
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="body-right">
+                    <div class="body-first">
+                        <div class="first-left">
+                            <h3>Khu nghỉ dưỡng topas Ecoldge Sapa</h3>
+                        
+                            <div class="star2">
+                                <span class="modify2">
+                                    <i class="bi bi-star-fill "></i>
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star-fill"></i>
+                                    <i class="bi bi-star-fill"></i>
+                                </span>
+                            </div>
+                            <div class="gps">
+                                <i class="bi bi-geo-alt-fill"></i>
+                                Thanh kim,Sapa,Lào Cai,Việt Nam
+                            </div>    
+                        </div>
+                        <div class="first-right">
+                            <span>Giá chỉ từ<span>
+                            <div class="price">
+                                <p>4.700.000 VNĐ</p>
+                            </div>
+                            <div class="last">
+                                <button class="button-first">Đặt ngay</button>
+                            </div>
+                        </div> 
+                                         
+                    </div>
+                                      
+                </div>
+            </section>
         </div>
         
-       
     </div>
 </body>
 </html>
