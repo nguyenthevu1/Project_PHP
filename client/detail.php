2eb6958bbf68e30c3e73964fa017fd70249dadb3
chithanh2001 <satthumaulanh2001@gmail.com>
2021-10-26 14:51:21 +0700

    day file

---
+++ client/detail.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Chi tiết</title>
+    <link href="css/detail.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
+</head>
+<body>
+    <div class="header">
+        <div class="container">
+            <div class="image">
+
+            </div>
+            <div class="list">
+                <ul>
+                    <li class="modify">
+                        <a href="#"><h2> Khách sạn<h2></a>                       
+                    </li>
+                    <li class="modify">
+                        <a href="#"><h2>Tours<h2></a>
+                    </li>
+                    <li class="modify">
+                        <a href="#"><h2>Vé máy bay</h2></a>
+                    </li>
+                    <li class="modify">
+                        <a href="#"><h2>Ăn uống</h2></a>
+                    </li>
+                    <li class="modify">
+                        <h2>...</h2>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        
+       
+    </div>
+</body>
+</html>
