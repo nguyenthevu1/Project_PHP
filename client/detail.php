8a1b3cba141149bf50f48b6aa39b306fa07e3ad3
chithanh2001 <satthumaulanh2001@gmail.com>
2021-10-28 11:47:46 +0700

    update detail

--- client/detail.php
+++ client/detail.php
@@ -3,6 +3,8 @@
 <head>
     <title>Chi tiết</title>
     <link href="css/detail.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <link rel="stylesheet" type="text/css" href="https://npmcdn.com/flatpickr/dist/themes/material_blue.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" rel="stylesheet">
 </head>
@@ -200,8 +202,8 @@
                                 <div class="datepicker">
                                     <div class="date-left">
                                     <i class="far fa-calendar-alt"></i>
-                                    <input type="date">
-                                    <input type="date">
+                                    <input id="myID" type="date" placeholder="Ngày đến">
+                                    <input id="myID" type="date" placeholder="Ngày đi">
                                     </div>
                                 </div>                            
                             </div>
@@ -211,12 +213,141 @@
                     </div>    
                     <div class="title">
                         <h3 class="font-size-16 font-bold vcolor-primary">Bảng giá Khu nghỉ dưỡng Topas Ecolodge Sapa</h3>
-                    </div>           
+                    </div>    
+                    
+                    <div class="search-2">
+                        <div class="search-detail-left">
+                            <div class="left-1">
+                                <input id="myID" type="datetime-local" placeholder="Ngày đến" style="height:50px;border-radius:5px;width:150px;">
+                            </div>
+                            <div class="left-2">
+                                <input id="myID" type="datetime-local" placeholder="Ngày đi" style="height:50px;border-radius:5px;">
+                            </div>
+                            
+                           
+                            
+                        </div>
+                        <div class="search-detail-mid">
+                                <input type="text" placeholder="tạm" style="height:50px; border-radius:5px;">
+                        </div>
+                        <div class="search-detail-right">
+                                <button>Tìm</button>
+                        </div>
+                    </div> 
+                    <div class="mini">
+                        <span>Trả góp chỉ từ <strong>412.000 VND</strong>/tháng qua thẻ tín dụng Visa, Mastercard, JCB  <a href="#" class="fix" >Xem chi tiết</a> </span>    
+                    </div>                
+                                       
+                    <div class="room">
+                        <div class="title-room">
+                            <table class="table">
+                                <thead class="fix-2">
+                                    <tr>
+                                        <th>Loại phòng</th>
+                                        <th>Các lựa chọn>
+                                        <th class="text-right">
+                                            "Tổng giá"
+                                            <br>
+                                        </th>
+                                        <th>Giữ phòng</th>
+                                    <tr>
+                                </thead>
+                                <tbody>
+                                    <tr class = "room-detail">
+                                        <td>
+                                            <div class="container-left">
+                                                <div class="header-left">
+                                                    <h3>Premium Executive<br> Bungalow</h3>
+                                                </div>
+                                                <div class="mid-left">
+                                                    <img src="images/premium-executive-bungalow-220x125.jpg">
+                                                </div>
+                                                <div class="footer-left">
+                                                    <p>Tối đa 3 người lớn | 2 người<br> lớn & 1 trẻ em </p>
+                                                    <p>Hướng núi</p>
+                                                    <p>29m²</p>
+                                                    <p>Phòng không hút thuốc</p>
+                                                    <p>Phòng cửa sổ mở</p>
+                                                    <p>1 Giường đôi | 2 giường đơn</p>
+                                                    <p>Ban công</p>
+                                                    <p>Vòi hoa sen</p>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        
+                                        <td class="condition">
+                                            <div class="condition-left">
+                                                <h3>Gồm ăn sáng + Voucher</h3>
+                                                <p>Miễn phí xe Limousine Topas<br>Moutain Express cho 2 người<br>lớn/phòng đưa đón từ Hà Nội cho<br>đặt phòng từ 2 đêm trong tuần<br>(tuỳ theo tình trạng chỗ, không áp<br>dụng Lễ Tết)</p>
+                                                <p>Tặng 1,000,000VND/phòng/<br>đêm sử dụng cho dịch vụ ăn uống</p>
+                                                <p>Miễn phí sử dụng xe đạp trong<br> khu nghỉ</p>
+                                                <p>Đặt từ 3 đêm giảm thêm 10%</p>
+                                                <p>Đã bao gồm thuế & phí</p>
+                                                <p>Giá rẻ (không hủy/thay đổi)</p>
+                                            </div>
+                                                
+                                        </td>
+                                        <td>
+                                            <div class="container-mid">
+                                                <h5>Giá 1 đêm cho 2 người</h5>
+                                                <a class ="color" href="#">4.698.000 VND </a>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="container-right">
+                                                <button>Đặt phòng</button>
+                                                <p>Xác nhận trong<br> 60 phút</p>
+                                            </div>
+                                        </td>
+                                    </tr>
+
+                                    <tr >
+                                        <td>
+                                            <div class="second-left">
+
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="rate">
+                                                <h3>Gồm ăn sáng</h3>
+                                                <p>1 giường cỡ queen và 1 sofa<br>giường đơn lớn</p>
+                                                <p>Phòng suite</p>
+                                                <p>1 giường cỡ queen và 1 sofa<br> giường đơn lớn</p>
+                                                <p>Bãi đậu xe tự phục vụ miễn phí</p>
+                                                <p>Bữa sáng miễn phí</p>
+                                                <p>Đã bao gồm thuế & phí</p>
+                                                <p>Giá rẻ (không hủy/thay đổi)</p>
+                                            </div>
+                                            
+                                        </td>
+                                        <td>
+                                            <div class="second-price">
+                                                <h5>Giá 1 đêm cho 2 người</h5>
+                                                <a class ="color" href="#">4.698.000 VND </a>
+                                                <p>Giá đặt online</p>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="second-order">
+                                                <button>Đặt phòng</button>
+                                                <p>Xác nhận ngay</p>
+                                            </div>
+                                        </td>
+
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
                 
-            </section>
+            </section>  
         </div>
         
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+    <script>
+        flatpickr("#myID", {});
+    </script>
 </body>
 </html>
