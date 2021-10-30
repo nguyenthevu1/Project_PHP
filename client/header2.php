759f85a7ae934356663532735df2c44b7ecb0148
chithanh2001 <satthumaulanh2001@gmail.com>
2021-10-30 21:15:33 +0700

    new header

---
+++ client/header2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Chi tiết</title>
+    <link href="css/list-room.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <link rel="stylesheet" type="text/css" href="https://npmcdn.com/flatpickr/dist/themes/material_blue.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="img-header">
+                <img src="images/gnb_logo_hotels.png">
+            </div>
+            
+            <a href="#"><i class="fa fa-globe" aria-hidden="true"></i>TRANG CHỦ</a>
+            <a href="#"><span class="span">PHÒNG<span></a>
+            <a href="#"><span class="span">DỊCH VỤ<span></a>
+            <a href="#"><span class="span">LIÊN HỆ<span></a>
+            <a href="#"><span class="span">PHÒNG<span></a>
+            <ul>
+                <li class="hover-me"><a href="#">Người đăng nhập</a>
+                    <div class="sub-menu-1">
+                        <ul>
+                            <li>
+                                <a href=#>Đăng xuất </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
